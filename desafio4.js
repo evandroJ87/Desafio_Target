0c38066fc34e713b95c464cfb26cbe652e32d025
Evandro <evandrosilva8744@gmail.com>
2023-02-28 03:17:44 -0300

    first commit

---
+++ desafio4.js
@@ -0,0 +1,7 @@
+const SP = Number(67.836,43);
+const RJ = Number(36.678,66);
+const MG = Number(29.229,88);
+const ES = Number(27.165,48);
+const Outros = Number(19.849,53);
+
+const total = SP+RJ+MG+ES+Outros;
